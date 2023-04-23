82d682369853c5a784b4e24b5ceb18abf136b486
Napster <rome.1205h@gmail.com>
2023-04-22 23:54:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const APIURL = "https://api.github.com/users/";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+
+
